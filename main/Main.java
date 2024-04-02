3c955ad605379377f82bd03972bf3d9236a97d36
Emisito <131873705+emisito@users.noreply.github.com>
2024-04-02 20:26:42 +0200

    Comenzado V1

---
+++ main/Main.java
@@ -0,0 +1,13 @@
+package main;
+/**
+ * @author emi
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Engine eng = new Engine();
+		eng.start();
+	}
+
+}
---
+++ main/OperaAndStack.java
@@ -0,0 +1,12 @@
+package main;
+
+public class OperaAndStack {
+	private final int MAX_STACK = 10;
+	private int[] Stack;
+	private int num_elems;
+	
+	public OperaAndStack(){
+		this.Stack = new int[MAX_STACK];
+		this.num_elems = 0;
+	}
+}

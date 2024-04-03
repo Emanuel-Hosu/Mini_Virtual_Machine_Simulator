4504187d7afd1bee5f8b41d8cf5cd6626e3c3c48
Emisito <131873705+emisito@users.noreply.github.com>
2024-04-03 17:03:07 +0200

    Agragando nuevas clases en V1

--- main/OperaAndStack.java
+++ main/OperaAndStack.java
@@ -1,12 +1,74 @@
 package main;
-
+/**
+ * @author emi
+ */
 public class OperaAndStack {
 	private final int MAX_STACK = 10;
 	private int[] Stack;
 	private int num_elems;
-	
+	/**
+	 * OperaAndStack
+	 */
 	public OperaAndStack(){
+		//num_elems ++?
 		this.Stack = new int[MAX_STACK];
-		this.num_elems = 0;
+		this.num_elems = -1;
+	}
+	/**
+	 * toString
+	 * @param cadena
+	 * @return cadena
+	 */
+	public String toString(String cadena) {
+		return cadena;
+	}
+	/**
+	 * isEmpty
+	 * @return
+	 */
+	public boolean isEmpty() {
+		//
+		if (this.num_elems < this.Stack.length && this.Stack[num_elems] != -1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	/**
+	 * push
+	 * @param num
+	 */
+	public void push(int num) {
+		//COMPRUEBA SI EL STACK ESTA LLENO
+		if (this.num_elems < this.Stack.length && this.Stack[num_elems] != -1) {
+			this.Stack[num_elems] = num;
+		}
+	}
+	/**
+	 * pop
+	 * @return 
+	 */
+	public int pop() {
+		int pop_num;
+		
+		if (isEmpty() == false) {
+			pop_num = Stack[num_elems];
+			
+			Stack[num_elems] --;
+			return pop_num;
+		}else {
+			return (Integer) null;
+		}
+	}
+	/**
+	 * getCima
+	 * @return
+	 */
+	public int getCima(){
+		if (isEmpty() == false) {
+			return Stack[num_elems];
+		}else {
+			return (Integer) null;
+		}
 	}
 }
